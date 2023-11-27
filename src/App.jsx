ae0ecf2c20405627ef72002a43c3314289915e81
GonzaloIvanVallone <GonzaloValloneIvan@gmail.com>
2023-11-27 15:40:45 -0300

    navbarv1.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home/Home';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route exact path={'/'} element={<Home/>}/>
           <Route exact path={'/register'} element={<Register/>}/>
           <Route exact path={'/login'} element={<Login/>}/>
+          <Route exact path={'/navbar'} element={<NavBar/>}/>
         </Routes>
       </div>
     </BrowserRouter>
