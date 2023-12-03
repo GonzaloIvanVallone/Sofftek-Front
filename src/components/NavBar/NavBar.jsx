87082a342d460e0f9dffa2cb72f291d12a8dbf17
GonzaloIvanVallone <GonzaloValloneIvan@gmail.com>
2023-12-03 00:56:01 -0300

    front reset circuit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import './Navbar.css';
+import './NavBar.css';
 import { filterByCategories, getAllCategories } from "../../redux/actions/indexActions";
 import { useNavigate } from 'react-router-dom';
 
