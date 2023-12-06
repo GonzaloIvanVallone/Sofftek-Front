fe142f7452e1c7e7cb85e5bccf288071e50e577c
Matias Perona <matiasperona81@gmail.com>
2023-12-06 16:58:28 -0300

    prueba

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -2,13 +2,43 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import "./ProductDetail.scss";
 import { useDispatch, useSelector } from "react-redux";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+import { addToCart } from "../../redux/actions/indexActions";
 
 export const ProductDetail = () => {
   const { id } = useParams();
   const allProducts = useSelector((state) => state.allProducts);
+  const dispatch = useDispatch();
+
   const product1 = allProducts.find(
     (product) => product.idProduct === parseInt(id, 10)
   );
+
+  const handleAddToCart = () => {
+    if (product1.productStock > 0) {
+      // Dispatch de la acción addToCart
+      dispatch(addToCart(product1));
+
+      // Muestra una notificación
+      Swal.fire({
+        title: "¡Producto agregado al carrito!",
+        icon: "success",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } else {
+      // Muestra una alerta si no hay suficiente stock
+      Swal.fire({
+        title: "¡Producto agotado!",
+        icon: "warning",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+    
+  };
+
 
   return (
     <div className="container-product-deteil mt-1 mb-3">
@@ -33,7 +63,7 @@ export const ProductDetail = () => {
         </div>
       </div>
       <div>
-        <button className="btn btn-success">Comprar</button>
+        <button className="btn btn-success" onClick={handleAddToCart}>Comprar</button>
       </div>
       <div></div>
     </div>
