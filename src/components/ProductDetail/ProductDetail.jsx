fc7ef0e5f8159a715856fd7c53c841a6944b3244
Juan Insua <insuajuancruz@gmail.com>
2023-12-08 03:00:44 -0300

    Update NotLoggin style

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -40,8 +40,8 @@ export const ProductDetail = () => {
     isLoggedIn
       ? navigate("/Buy", { state: { product } })
       : navigate("/NotLoggin", {
-          state: "no puede comprar productos sin antes ingresar a su cuenta",
-        });
+        state: "You cannot purchase products without first logging into your account.",
+      });
     // navigate("/Buy", { state: { product } });
   };
 
@@ -49,40 +49,42 @@ export const ProductDetail = () => {
     isLoggedIn
       ? handleAddToCart()
       : navigate("/NotLoggin", {
-          state:
-            "no puede agregar productos al carrito sin antes ingresar a su cuenta",
-        });
+        state:
+          "You cannot add products to the cart without first logging into your account.",
+      });
   };
 
   return (
     <div className="container-product-deteil mt-1 mb-3">
-      <div className="container-facher">
-        <div className="container-information">
-          <div className="container-img">
-            <img
-              className="img"
-              src={product.productImg}
-              alt="imagen del producto"
-            />
-          </div>
-          <div className="container-card-info">
-            <h3>{product.productName}</h3>
-            <p>$ {product.productPrice}</p>
-            <p>Stock: {product.productStock}</p>
-            <p className="text-start">{product.description}</p>
+      <div className="Product-Detail">
+        <div className="container-facher">
+          <div className="container-information">
+            <div className="container-img">
+              <img
+                className="img"
+                src={product.productImg}
+                alt="imagen del producto"
+              />
+            </div>
+            <div className="container-card-info">
+              <h3>{product.productName}</h3>
+              <p>$ {product.productPrice}</p>
+              <p>Stock: {product.productStock}</p>
+              <p className="text-start">{product.description}</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="container_buttons">
-        <div className="buttons_actions">
-          <button className="btn btn-success" onClick={handleBuyClick}>
-            Buy
-          </button>
-        </div>
-        <div className="buttons_actions">
-          <button className="btn btn-success" onClick={handleAddCartClick}>
-            Add to Cart
-          </button>
+        <div className="container_buttons">
+          <div className="buttons_actions">
+            <button className="btn btn-success" onClick={handleBuyClick}>
+              Buy
+            </button>
+          </div>
+          <div className="buttons_actions">
+            <button className="btn btn-success" onClick={handleAddCartClick}>
+              Add to Cart
+            </button>
+          </div>
         </div>
       </div>
     </div>
